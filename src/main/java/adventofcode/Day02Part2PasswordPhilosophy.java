f32d2d248a9007e859790937bdfba66819a7df96
Rachana Devi <rachanadevi18@gmail.com>
2020-12-12 01:49:32 +0530

    [Rachana][Day02.2] Add. Test for an array input and input from file

--- src/main/java/adventofcode/Day02Part2PasswordPhilosophy.java
+++ src/main/java/adventofcode/Day02Part2PasswordPhilosophy.java
@@ -1,22 +1,28 @@
 package adventofcode;
 
+import java.util.List;
+
 public class Day02Part2PasswordPhilosophy {
 
     public static boolean satisfiesPassword(String rule) {
         var inputOutputSplit = rule.split(":");
         var charPosition = inputOutputSplit[0].split(" ")[0].split("-");
         var appliedAlphabet = inputOutputSplit[0].split(" ")[1].charAt(0);
-        var firstPosition = Integer.parseInt(charPosition[0]);
-        var secondPosition = Integer.parseInt(charPosition[1]);
+        var firstPosition = Integer.parseInt(charPosition[0]) - 1;
+        var secondPosition = Integer.parseInt(charPosition[1]) - 1;
         var count = 0;
         var password = inputOutputSplit[1].trim();
         final var passwordCharacters = password.split("");
-        return (password.charAt(firstPosition) != password.charAt(secondPosition)
-                && (
-                password.charAt(firstPosition) == appliedAlphabet
-                        ||
-                        password.charAt(secondPosition) == appliedAlphabet));
-
+        var bothNotInSamePosition = password.charAt(firstPosition) != password.charAt(secondPosition);
+        var firstPositionHavingAppliedAlphabet = password.charAt(firstPosition) == appliedAlphabet;
+        var secondPositionHavingAppliedAlphabet = password.charAt(secondPosition) == appliedAlphabet;
+        var finalAnswer = (bothNotInSamePosition && (firstPositionHavingAppliedAlphabet
+                || secondPositionHavingAppliedAlphabet));
+        return finalAnswer;
     }
 
+
+    public static int correctPasswords(List<String> passwordRules) {
+        return (int) passwordRules.stream().filter(Day02Part2PasswordPhilosophy::satisfiesPassword).count();
+    }
 }
--- src/test/java/adventofcode/Day02Part2PasswordPhilosophyUnitTest.java
+++ src/test/java/adventofcode/Day02Part2PasswordPhilosophyUnitTest.java
@@ -2,6 +2,13 @@ package adventofcode;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static adventofcode.Day02Part2PasswordPhilosophy.correctPasswords;
 import static adventofcode.Day02Part2PasswordPhilosophy.satisfiesPassword;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -13,5 +20,21 @@ public class Day02Part2PasswordPhilosophyUnitTest {
         var rule = "3-4 t: dtttt";
 
         assertThat(satisfiesPassword(rule), is(false));
+    }
+
+    @Test
+    public void shouldPassPasswordConditionsGivenAnArrayOfTwoPasswordRules() {
+        var passwordRules = List.of("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc");
+
+        assertThat(correctPasswords(passwordRules), is(1));
+    }
+
+    @Test
+    public void shouldPassPasswordConditionsGivenManyPasswordRulesFromAFile() throws IOException {
+        var passwordRules = new ArrayList<>(Files
+                .readAllLines(Paths
+                        .get("src/test/resources/day2_password_philosophy.txt")));
+
+        assertThat(correctPasswords(passwordRules), is(352));
     }
 }
