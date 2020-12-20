b6e67062aad6f94b7ef9b48e4d48f815b91fdfbb
Rachana Devi <rachanadevi18@gmail.com>
2020-12-20 10:44:27 +0530

    [Rachana][Day04.1] Add. Update getPassports logic which now returns String[] now

--- src/main/java/adventofcode/utilities/Day04Part1PassportProcessingUtilities.java
+++ src/main/java/adventofcode/utilities/Day04Part1PassportProcessingUtilities.java
@@ -6,21 +6,22 @@ import java.nio.file.Paths;
 
 public class Day04Part1PassportProcessingUtilities {
 
-    public static String getPassports(String filePath) throws IOException {
+    public static String[] getPassports(String filePath) throws IOException {
         var fileReader = Files.readString(Paths.get(filePath));
-        return fileReader.replace("\n", " ");
+        var passports = splitByTwoNewlines(fileReader);
+        return removeNewLines(passports);
     }
 
     static String[] removeNewLines(String[] passports) {
         String[] newPassports = new String[passports.length];
         int i = 0;
         for (String passport : passports) {
-            newPassports[i++] = passport.replace("\n", "");
+            newPassports[i++] = passport.replace("\n", " ");
         }
         return newPassports;
     }
 
-    public static String[] splitByTwoNewlines(String fileReader) {
+    static String[] splitByTwoNewlines(String fileReader) {
         return fileReader.split("\n\n");
     }
 }
--- src/test/java/adventofcode/utilities/Day04Part1PassportProcessingUtilitiesUnitTest.java
+++ src/test/java/adventofcode/utilities/Day04Part1PassportProcessingUtilitiesUnitTest.java
@@ -1,6 +1,7 @@
 package adventofcode.utilities;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.io.IOException;
 
@@ -11,18 +12,20 @@ import static org.hamcrest.core.Is.is;
 public class Day04Part1PassportProcessingUtilitiesUnitTest {
 
     @Test
-    public void shouldGetPassportsFromFileInStringHavingOnePassportDetail() throws IOException {
+    public void shouldGetPassportsFromFileHavingOnePassportDetail() throws IOException {
         var passports = getPassports("src/test/resources/day_4_passport_processing_one_correct_passport.txt");
 
-        assertThat(passports.contains("\n"), is(false));
+        assertThat(passports[0], is("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"));
     }
 
     @Test
-    public void shouldGetPassportsFromFileInStringHavingTwoPassportDetails() throws IOException {
-        var passports = getPassports("src/test/resources/day_4_passport_processing_one_correct_passport_and_two_passport_details.txt");
-        System.out.println(passports);
+    public void shouldGetPassportsFromFileHavingTwoPassportDetails() throws IOException {
+        var passports = getPassports("src/test/resources/day_4_passport_processing_one_correct_passport_from_two_passport_details.txt");
 
-        assertThat(passports.contains("\n"), is(false));
+        Assertions.assertAll(
+                () -> assertThat(passports[0], is("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm")),
+                () -> assertThat(passports[1], is("hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm"))
+        );
     }
 
     @Test
