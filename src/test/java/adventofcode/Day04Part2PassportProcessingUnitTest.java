0033d49a184f0cacbeb42370f3505f19fde6e9af
Rachana Devi <rachanadevi18@gmail.com>
2020-12-22 00:05:35 +0530

    [Rachana][Day04.2] Add. should valid passports given an array of passports

--- src/test/java/adventofcode/Day04Part2PassportProcessingUnitTest.java
+++ src/test/java/adventofcode/Day04Part2PassportProcessingUnitTest.java
@@ -14,4 +14,12 @@ public class Day04Part2PassportProcessingUnitTest {
 
         assertThat(validPassports(invalidPassports), is(0));
     }
+
+    @Test
+    public void shouldReturnOneValidPassportGivenTwoPassports() {
+        String[] passports = {"eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f"};
+
+        assertThat(validPassports(passports), is(1));
+    }
 }
