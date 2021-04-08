1f8ad4d7001a48ebee6fb90ad5f389a3d9349be7
Rachana Devi <rachanadevi18@gmail.com>
2021-04-09 01:22:23 +0530

    [Rachana] Adds Utility FileReader to read file content separated by blank lines

---
+++ src/main/java/adventofcode/utilities/FileReader.java
@@ -0,0 +1,27 @@
+package adventofcode.utilities;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileReader {
+
+    public static String[] contentSeparatedByABlankLine(String filePath) throws IOException {
+        var fileAsString = Files.readString(Paths.get(filePath));
+        var contents = splitByTwoNewlines(fileAsString);
+        return removeNewLines(contents, " ");
+    }
+
+    public static String[] removeNewLines(String[] contents, String replacement) {
+        String[] contentArray = new String[contents.length];
+        int i = 0;
+        for (String content : contents) {
+            contentArray[i++] = content.replace("\n", replacement);
+        }
+        return contentArray;
+    }
+
+    static String[] splitByTwoNewlines(String fileAsString) {
+        return fileAsString.split("\n\n");
+    }
+}
---
+++ src/test/java/adventofcode/utilities/FileReaderUnitTest.java
@@ -0,0 +1,46 @@
+package adventofcode.utilities;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class FileReaderUnitTest {
+
+    @Test
+    public void shouldGetAllContentFromFileHavingOneContent() throws IOException {
+        var passports = FileReader.contentSeparatedByABlankLine("src/test/resources/day_4_passport_processing_one_correct_passport.txt");
+
+        assertThat(passports[0], is("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"));
+    }
+
+    @Test
+    public void shouldReturnAllContentSeparatedByBlankLineInBetween() throws IOException {
+        var passports = FileReader.contentSeparatedByABlankLine("src/test/resources/day_4_passport_processing_one_correct_passport_from_two_passport_details.txt");
+
+        Assertions.assertAll(
+                () -> assertThat(passports[0], is("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm")),
+                () -> assertThat(passports[1], is("hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm"))
+        );
+    }
+
+    @Test
+    public void shouldSplitAStringHavingDelimiterAsTwoLines() {
+        String stringHavingTwoNewLines = "firstString\n" +
+                "\n" +
+                "secondString";
+
+        assertThat(FileReader.splitByTwoNewlines(stringHavingTwoNewLines), is(new String[]{"firstString", "secondString"}));
+    }
+
+    @Test
+    public void shouldRemoveNewlinesInAString() {
+        String[] stringHavingTwoNewLines = {"firstString\n"};
+
+        assertThat(FileReader.removeNewLines(stringHavingTwoNewLines, " ")[0].contains("\n"), is(false));
+    }
+
+}
